0c7d94558a4e1e35217ba3c819f5a97c7dcc3d46
therahulsonawane <rahulsonwane7066@gmail.com>
2024-10-08 08:56:34 +0530

    Learn about batch processing

--- JDBCDemo/src/App.java
+++ JDBCDemo/src/App.java
@@ -18,6 +18,8 @@ public class App {
             Connection connection = DriverManager.getConnection(url, username, password); //connecting to databas
 
             // *** Data inserted into database using prepareStatement ***
+
+
             // String query = "INSERT INTO students(name,age,marks) VALUES(?,?,?)";
             // // statement statement = connection.createStatement();
             // PreparedStatement pst = connection.prepareStatement(query);
@@ -41,7 +43,13 @@ public class App {
             // } else {
             //     System.out.println("Marks not found");
             // }
+
+
+
+
+
             // *** Data update from database using prepareStatement ***
+
             // String query = "UPDATE students SET marks = ?,age = ? WHERE  id = ?";
             // PreparedStatement preparedStatement = connection.prepareStatement(query);
             // preparedStatement.setDouble(1, 99.5);
@@ -54,15 +62,88 @@ public class App {
             //     System.out.println("Data is not updated successfully");
             // }
             // *** Data Delete from database using prepareStatement ***
-            String query = "DELETE FROM students WHERE id =? ";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, 1);
-            int rowsAffected = preparedStatement.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Data deleted successfully");
-            } else {
-                System.out.println("Data is not deleted successfully");
-            }
+            // String query = "DELETE FROM students WHERE id =? ";
+            // PreparedStatement preparedStatement = connection.prepareStatement(query);
+            // preparedStatement.setInt(1, 1);
+            // int rowsAffected = preparedStatement.executeUpdate();
+            // if (rowsAffected > 0) {
+            //     System.out.println("Data deleted successfully");
+            // } else {
+            //     System.out.println("Data is not deleted successfully");
+            // }
+
+
+
+
+
+
+            // ####### Batch Processing using statement #######
+            
+            // Statement statement = connection.createStatement();
+            // Scanner sc = new Scanner(System.in);
+            // while (true) {
+            //     System.out.println("Enter name: ");
+            //     String name = sc.next();
+            //     System.out.println("Enter age: ");
+            //     int age = sc.nextInt();
+            //     System.out.println("Enter Marks: ");
+            //     double marks = sc.nextDouble();
+            //     System.out.print("Enter More Data (Y/N)");
+            //     String choice = sc.next();
+            //     String query = String.format("INSERT INTO students(name,age,marks) VALUES('%s',%d,%f)", name, age, marks);
+            //     statement.addBatch(query);
+            //     if (choice.toUpperCase().equals("N")) {
+            //         break;
+            //     }
+            // }
+            // int[] arr = statement.executeBatch();
+            // for (int i = 0; i < arr.length; i++) {
+            //     if (arr[i] == 0) {
+            //         System.out.println("Query: " + i + "  not executed Sucessfully");
+            //     }
+            // }
+
+
+
+            
+
+
+            // ####### Batch Processing using prepared statement #######
+
+            // String query = "INSERT INTO students(name,age,marks) VALUES(?,?,?)";
+            // PreparedStatement preparedStatement = connection.prepareStatement(query);
+            // Scanner sc = new Scanner(System.in);
+            // while (true) {
+            //     System.out.println("Enter name: ");
+            //     String name = sc.next();
+            //     System.out.println("Enter age: ");
+            //     int age = sc.nextInt();
+            //     System.out.println("Enter Marks: ");
+            //     double marks = sc.nextDouble();
+            //     System.out.print("Enter More Data (Y/N)");
+            //     String choice = sc.next();
+
+            //     preparedStatement.setString(1, name);
+            //     preparedStatement.setInt(2, age);
+            //     preparedStatement.setDouble(3, marks);
+
+            //     preparedStatement.addBatch();
+            //     if (choice.toUpperCase().equals("N")) {
+            //         break;
+            //     }
+            // }
+            // int[] arr = preparedStatement.executeBatch();
+
+            // for (int i = 0; i < arr.length; i++) {
+            //     if (arr[i] == 0) {
+            //         System.out.println("Query: " + i + "  not executed Sucessfully");
+            //     }
+            // }
+
+
+
+            // *********** Transaction Handleing ****************************
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
